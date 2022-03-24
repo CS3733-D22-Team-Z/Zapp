c4b0a275ec8129dfa337ed8ef3570655bd2959cd
Daniel Correa <dcorrea012@gmail.com>
2022-03-24 16:47:32 -0400

    Created Medical Equipment Request Page

---
+++ src/main/java/edu/wpi/teamZ/controllers/MedicalEquipmentDelivery.java
@@ -0,0 +1,23 @@
+package edu.wpi.teamZ.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.teamZ.App;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MedicalEquipmentDelivery {
+  @FXML private JFXButton backButton;
+
+  @FXML
+  public void onBackButtonClicked(ActionEvent event) throws IOException {
+    Stage mainStage = (Stage) backButton.getScene().getWindow();
+    Parent root = FXMLLoader.load(App.class.getResource("views/app.fxml"));
+    Scene scene = new Scene(root);
+    mainStage.setScene(scene);
+  }
+}
