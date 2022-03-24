8a2d5578787681df6a7c8b580dd1a50c5f4c1336
Claire Higginson <clurr555@gmail.com>
2022-03-24 16:47:54 -0400

    created patient meal request page w/ working back button

---
+++ src/main/java/edu/wpi/teamZ/controllers/AppController.java
@@ -0,0 +1,22 @@
+package edu.wpi.teamZ.controllers;
+
+import edu.wpi.teamZ.App;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class AppController {
+  @FXML private Button backButton;
+
+  @FXML
+  void backToDashboard() throws IOException {
+    Stage primaryStage = (Stage) backButton.getScene().getWindow();
+    Parent root = FXMLLoader.load(App.class.getResource("views/app.fxml"));
+    Scene scene = new Scene(root);
+    primaryStage.setScene(scene);
+  }
+}
