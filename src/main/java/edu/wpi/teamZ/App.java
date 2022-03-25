8c6a23ebf390c0f51f9513b92157905189a90a8e
nkuchipudi2020 <nkuchipudi@wpi.edu>
2022-03-25 18:44:02 -0400

    Added chevron png for landing page

--- src/main/java/edu/wpi/teamZ/App.java
+++ src/main/java/edu/wpi/teamZ/App.java
@@ -5,6 +5,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,8 +21,15 @@ public class App extends Application {
 
   @Override
   public void start(Stage primaryStage) throws IOException {
+    Text text = new Text(1.0, 2.0, "Custom Font");
+    Font font = Font.loadFont("resources/fonts/Montserrat-Bold.ttf", 45);
+    text.setFont(font);
+    text.setFill(Color.BROWN);
+    text.setStroke(Color.BLUEVIOLET);
+    text.setStrokeWidth(0.5);
     Parent root = FXMLLoader.load(App.class.getResource("views/app.fxml"));
     Scene scene = new Scene(root);
+    primaryStage.setTitle("Custom Font");
     primaryStage.setScene(scene);
     primaryStage.show();
   }
