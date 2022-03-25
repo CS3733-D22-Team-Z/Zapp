7827ea6a68e25d2d51d7a6a6c49992aa005719ba
Daniel Correa <dcorrea012@gmail.com>
2022-03-24 21:12:11 -0400

    updated Equipment service page
    Added text, labels, and choice menu field

--- src/main/java/edu/wpi/teamZ/controllers/MedicalEquipmentDelivery.java
+++ src/main/java/edu/wpi/teamZ/controllers/MedicalEquipmentDelivery.java
@@ -2,6 +2,7 @@ package edu.wpi.teamZ.controllers;
 
 import com.jfoenix.controls.JFXButton;
 import edu.wpi.teamZ.App;
+import java.awt.*;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,6 +13,12 @@ import javafx.stage.Stage;
 
 public class MedicalEquipmentDelivery {
   @FXML private JFXButton backButton;
+  @FXML private Label header;
+  @FXML private Label objectBodyText;
+  @FXML private Label roomNumberLabel;
+  @FXML private Label equipmentLabel;
+  @FXML private TextField roomNumberTextField;
+  @FXML private TextField equipmentTextField;
 
   @FXML
   public void onBackButtonClicked(ActionEvent event) throws IOException {
