888af79a08b4c24a13d8ba4465cd611d26cffec6
Alex Brattstrom <abrattstrom@wpi.edu>
2022-03-28 12:35:03 -0400

    Medical Equipment Delivery. ADDED: Reset and submit buttons. Reset functionality. Submit testing output.

--- src/main/java/edu/wpi/teamZ/controllers/MedicalEquipmentDelivery.java
+++ src/main/java/edu/wpi/teamZ/controllers/MedicalEquipmentDelivery.java
@@ -2,23 +2,33 @@ package edu.wpi.teamZ.controllers;
 
 import com.jfoenix.controls.JFXButton;
 import edu.wpi.teamZ.App;
-import java.awt.*;
 import java.io.IOException;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class MedicalEquipmentDelivery {
   @FXML private JFXButton backButton;
+  @FXML private JFXButton resetButton;
+  @FXML private JFXButton submitButton;
   @FXML private Label header;
   @FXML private Label objectBodyText;
   @FXML private Label roomNumberLabel;
   @FXML private Label equipmentLabel;
-  @FXML private TextField roomNumberTextField;
-  @FXML private TextField equipmentTextField;
+  @FXML private TextField enterRoomNumber;
+  @FXML private ChoiceBox equipmentDropDown;
+
+  @FXML
+  public void initialize() {
+    equipmentDropDown.setItems(FXCollections.observableArrayList("Bed", "IV", "Pillow"));
+  }
 
   @FXML
   public void onBackButtonClicked(ActionEvent event) throws IOException {
@@ -26,5 +36,16 @@ public class MedicalEquipmentDelivery {
     Parent root = FXMLLoader.load(App.class.getResource("views/app.fxml"));
     Scene scene = new Scene(root);
     mainStage.setScene(scene);
+  }
+
+  @FXML
+  public void onResetButtonClicked(ActionEvent event) throws IOException {
+    enterRoomNumber.clear();
+    equipmentDropDown.setValue(null);
+  }
+
+  public void onSubmitButtonClicked(ActionEvent actionEvent) {
+    System.out.println("Room Number: " + enterRoomNumber.getText());
+    System.out.println("Equipment Selected: " + equipmentDropDown.getValue());
   }
 }
